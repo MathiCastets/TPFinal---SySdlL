fb9235c300086ab2bf27bec3152b094c4ec25abf
MathiCastets <mathicastets@gmail.com>
2025-10-19 13:46:45 -0300

    base funcional - empezar a trabajar a partir de esto

---
+++ prueba.m
@@ -0,0 +1,7 @@
+inicio
+    leer(A, B);
+    Suma := A + B;
+    Resta := A - B;
+    Resultado := Suma + Resta - 100;
+    escribir(Suma, Resta, Resultado);
+fin
--- prueba_cte.m
+++
@@ -1,6 +0,0 @@
-inicio
-  leer(a);
-  leer(b);
-  resultado := a + b - 10;
-  escribir(resultado);
-fin
